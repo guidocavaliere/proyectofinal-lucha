5f95307b372049bf97f9b5ae50b4ca23930825cf
Guido Cavaliere <guido.a.cavaliere@gmail.com>
2022-09-14 23:27:28 -0300

    Proyecto Final Lucha: Movimiento con listener

--- index.js
+++ index.js
@@ -6,13 +6,14 @@ canvas.height = 579
 
 c.fillRect(0, 0, canvas.width, canvas.height)
 
-const gravedad = 0.2
+const gravedad = 0.7
 
 class Sprite {
     constructor({ posicion, velocidad }) {
         this.posicion = posicion
         this.velocidad = velocidad
         this.altura = 150
+        this.lastKey
     }
 
     //draw es uno de mis metodos de mi clase sprite
@@ -24,7 +25,8 @@ class Sprite {
     update() {
         this.draw()
         //this.posicion.y = this.posicion.y + 10 -- lo de abajo es mas simplificado
-        this.posicion.y += this.velocidad.y
+        this.posicion.x += this.velocidad.x
+        this.posicion.y += this.velocidad.y       
         if (this.posicion.y + this.altura + this.velocidad.y >= canvas.height)
            {this.velocidad.y = 0}
            else 
@@ -52,6 +54,31 @@ const oponente = new Sprite ({
 
 console.log(jugador);
 
+const keys = {
+    //jugador
+    a:{
+        pressed: false
+    },
+    d:{
+        pressed: false
+    },
+    w:{
+        pressed: false
+    },
+    //oponente
+    ArrowLeft:{
+        pressed: false
+    },
+    ArrowRight:{
+        pressed: false
+    },
+    ArrowUp:{
+        pressed: false
+    }
+
+}
+
+
 function animate () {
     window.requestAnimationFrame(animate)
     //llamo a un metodo de mi canvas
@@ -62,10 +89,86 @@ function animate () {
     jugador.update()
     oponente.update()
     //console.log('go');
+
+    jugador.velocidad.x = 0
+    oponente.velocidad.x = 0
+
+    //movimiento jugador
+    if (keys.a.pressed && jugador.lastKey === 'a') {
+        jugador.velocidad.x = -5
+    } else if (keys.d.pressed && jugador.lastKey === 'd') {
+        jugador.velocidad.x = 5
+    }
+
+    //movimiento oponente
+    if (keys.ArrowLeft.pressed && oponente.lastKey === 'ArrowLeft') {
+        oponente.velocidad.x = -5
+    } else if (keys.ArrowRight.pressed && oponente.lastKey === 'ArrowRight') {
+        oponente.velocidad.x = 5
+    }
      
 }
 
 animate()
+
+
+
+// event listeners. esto me servira para mover a los pj
+
+// keydown = apreto las teclas
+// keyup = suelto las teclas
+window.addEventListener('keydown', (event) => {
+    console.log(event.key)
+    switch(event.key) {
+        //jugador
+        case 'd': keys.d.pressed = true
+        jugador.lastKey = 'd'
+        break
+        case 'a': keys.a.pressed = true
+        jugador.lastKey = 'a'
+        break
+        case 'w': jugador.velocidad.y = -20
+        break
+
+        //oponente
+        case 'ArrowRight': keys.ArrowRight.pressed = true
+        oponente.lastKey = 'ArrowRight'
+        break
+        case 'ArrowLeft': keys.ArrowLeft.pressed = true
+        oponente.lastKey = 'ArrowLeft'
+        break
+        case 'ArrowUp': oponente.velocidad.y = -20
+        break
+    }
+
+    console.log(event.key);
+
+})
+
+window.addEventListener('keyup', (event) => {
+    switch(event.key) {
+        //jugador
+        case 'd': keys.d.pressed = false
+        break
+        case 'a': keys.a.pressed = false
+        break
+        case 'w': keys.w.pressed = false
+        break
+
+        //oponente
+        case 'ArrowRight': keys.ArrowRight.pressed = false
+        break
+        case 'ArrowLeft': keys.ArrowLeft.pressed = false
+        break
+        case 'ArrowUp': keys.w.pressed = false
+        break
+    }
+
+    console.log(event.key);
+
+})
+
+
 
 
 
